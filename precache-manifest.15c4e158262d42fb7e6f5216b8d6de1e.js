192f28b5125e67f61be4bed18347bab78d130082
Taylor Radtke <taylorradtke@gmail.com>
2019-12-23 22:42:59 -0500

    Updates

---
+++ precache-manifest.15c4e158262d42fb7e6f5216b8d6de1e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156876a0dd04aeff23c4261c0efd1d2c",
+    "url": "/react-house-hunter/index.html"
+  },
+  {
+    "revision": "abeb9176cdd97bcc91ea",
+    "url": "/react-house-hunter/static/js/2.016d1cdb.chunk.js"
+  },
+  {
+    "revision": "bed9c65c3eb5387e1c6a685bca244ca0",
+    "url": "/react-house-hunter/static/js/2.016d1cdb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91fd88ef5c0e27f7eb3a",
+    "url": "/react-house-hunter/static/js/main.16df844a.chunk.js"
+  },
+  {
+    "revision": "b0f6d97326ab68f23658",
+    "url": "/react-house-hunter/static/js/runtime-main.70bc7f4f.js"
+  },
+  {
+    "revision": "7e15d4cacb03d297d89b4f2dee74bbd7",
+    "url": "/react-house-hunter/static/media/bathtub.7e15d4ca.svg"
+  },
+  {
+    "revision": "211d4eba21eb84ca7aa6ebe7ced9ce78",
+    "url": "/react-house-hunter/static/media/bed-left-side.211d4eba.svg"
+  },
+  {
+    "revision": "f1c88fdfa1337e1aebdfb2605960d3cb",
+    "url": "/react-house-hunter/static/media/car-compact.f1c88fdf.svg"
+  },
+  {
+    "revision": "10ca4c62ad29b3624555f0d5c1db43d5",
+    "url": "/react-house-hunter/static/media/house-location-pin.10ca4c62.svg"
+  },
+  {
+    "revision": "2a7b5a9c3df9eae24f82bfb7d6930051",
+    "url": "/react-house-hunter/static/media/location-map.2a7b5a9c.svg"
+  },
+  {
+    "revision": "7500641adc4a03cc8d3b05cabae31312",
+    "url": "/react-house-hunter/static/media/tn_property01.7500641a.jpg"
+  },
+  {
+    "revision": "8538b3e04ab2258af03334a711112510",
+    "url": "/react-house-hunter/static/media/tn_property02.8538b3e0.jpg"
+  },
+  {
+    "revision": "38089d105bea5b18f9432395161f3aaf",
+    "url": "/react-house-hunter/static/media/tn_property03.38089d10.jpg"
+  },
+  {
+    "revision": "fa4d96f5573e41c77cf0d9c145261a8f",
+    "url": "/react-house-hunter/static/media/tn_property04.fa4d96f5.jpg"
+  },
+  {
+    "revision": "3595b09866ff29d1e21bb7a29bc103eb",
+    "url": "/react-house-hunter/static/media/tn_property05.3595b098.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-house-hunter/precache-manifest.15c4e158262d42fb7e6f5216b8d6de1e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-house-hunter/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
