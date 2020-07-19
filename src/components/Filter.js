aab9ac59e64abb12e77511a0bb56a0bfabeaaf99
Taylor Radtke <taylorradtke@gmail.com>
2020-07-19 15:53:35 -0400

    cleaned up some old logic

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -12,11 +12,13 @@ const Filter = ({ setState, toggleFilter, filterIsVisible }) => {
   const ref = useRef();
   const handleFilterChange = ({ target }) => {
     const { value, name } = target;
+
     setState(prev => {
       const copy = { ...prev };
       copy[name] = value;
 
       const {
+        properties,
         filterBedrooms,
         filterBathrooms,
         filterCars,
@@ -24,23 +26,17 @@ const Filter = ({ setState, toggleFilter, filterIsVisible }) => {
         priceTo,
       } = copy;
 
-      copy.filteredProperties = copy.properties
-        .filter(property => {
-          const { bedrooms, bathrooms, carSpaces, price } = property;
-          const match =
+      const filteredProperties = properties
+        .filter(({ bedrooms, bathrooms, carSpaces, price }) => {
+          return (
             (bedrooms === parseInt(filterBedrooms, 10) ||
               filterBedrooms === "any") &&
             (bathrooms === parseInt(filterBathrooms, 10) ||
               filterBathrooms === "any") &&
             (carSpaces === parseInt(filterCars, 10) || filterCars === "any") &&
             price >= priceFrom &&
-            price <= priceTo;
-
-          if (match) {
-            return property;
-          }
-
-          return false;
+            price <= priceTo
+          );
         })
         .sort((a, b) => {
           if (copy.filterSort === "0") {
@@ -54,18 +50,22 @@ const Filter = ({ setState, toggleFilter, filterIsVisible }) => {
           return false;
         });
 
-      copy.activeProperty = isArrayEmpty(copy.filteredProperties)
-        ? copy.properties[0]
-        : copy.filteredProperties[0];
+      const [activeProperty] = isArrayEmpty(filteredProperties)
+        ? properties
+        : filteredProperties;
 
-      copy.isFiltering =
-        copy.filterBedrooms !== "any" ||
-        copy.filterBathrooms !== "any" ||
-        copy.filterCars !== "any" ||
-        copy.priceFrom !== "0" ||
-        copy.priceTo !== "1000001" ||
-        copy.filterSort !== "any";
-      return copy;
+      return {
+        ...copy,
+        isFiltering:
+          copy.filterBedrooms !== "any" ||
+          copy.filterBathrooms !== "any" ||
+          copy.filterCars !== "any" ||
+          copy.priceFrom !== "0" ||
+          copy.priceTo !== "1000001" ||
+          copy.filterSort !== "any",
+        activeProperty,
+        filteredProperties,
+      };
     });
   };
 
@@ -79,10 +79,13 @@ const Filter = ({ setState, toggleFilter, filterIsVisible }) => {
             name="filterBedrooms"
             onChange={handleFilterChange}
           >
-            <option value="any">Any</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
+            {["any", 1, 2, 3].map((value, idx) => {
+              return (
+                <option key={`filterBedrooms-${value}`} value={value}>
+                  {idx === 0 ? "Any" : value}
+                </option>
+              );
+            })}
           </select>
         </label>
       </StyledFilterBox>
@@ -94,9 +97,13 @@ const Filter = ({ setState, toggleFilter, filterIsVisible }) => {
             name="filterBathrooms"
             onChange={handleFilterChange}
           >
-            <option value="any">Any</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
+            {["any", 1, 2].map((value, idx) => {
+              return (
+                <option key={`filterBathrooms-${value}`} value={value}>
+                  {idx === 0 ? "Any" : value}
+                </option>
+              );
+            })}
           </select>
         </label>
       </StyledFilterBox>
@@ -108,10 +115,13 @@ const Filter = ({ setState, toggleFilter, filterIsVisible }) => {
             name="filterCars"
             onChange={handleFilterChange}
           >
-            <option value="any">Any</option>
-            <option value="0">0</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
+            {["any", 0, 1, 2].map((value, idx) => {
+              return (
+                <option key={`filterCars-${value}`} value={value}>
+                  {idx === 0 ? "Any" : value}
+                </option>
+              );
+            })}
           </select>
         </label>
       </StyledFilterBox>
@@ -119,12 +129,13 @@ const Filter = ({ setState, toggleFilter, filterIsVisible }) => {
         <label htmlFor="priceFrom">
           Min Price
           <select id="priceFrom" name="priceFrom" onChange={handleFilterChange}>
-            <option value="0">Any</option>
-            <option value="500000">{priceFormat(500000)}</option>
-            <option value="600000">{priceFormat(600000)}</option>
-            <option value="700000">{priceFormat(700000)}</option>
-            <option value="800000">{priceFormat(800000)}</option>
-            <option value="900000">{priceFormat(900000)}</option>
+            {[0, 500000, 600000, 700000, 800000, 900000].map((value, idx) => {
+              return (
+                <option key={`priceFrom-${value}`} value={value}>
+                  {idx === 0 ? "Any" : priceFormat(value)}
+                </option>
+              );
+            })}
           </select>
         </label>
       </StyledFilterBox>
@@ -132,12 +143,15 @@ const Filter = ({ setState, toggleFilter, filterIsVisible }) => {
         <label htmlFor="priceTo">
           Max Price
           <select id="priceTo" name="priceTo" onChange={handleFilterChange}>
-            <option value="1000001">Any</option>
-            <option value="600000">{priceFormat(600000)}</option>
-            <option value="700000">{priceFormat(700000)}</option>
-            <option value="800000">{priceFormat(800000)}</option>
-            <option value="900000">{priceFormat(900000)}</option>
-            <option value="1000000">{priceFormat(1000000)}</option>
+            {[1000001, 600000, 700000, 800000, 900000, 1000000].map(
+              (value, idx) => {
+                return (
+                  <option key={`priceTo-${value}`} value={value}>
+                    {idx === 0 ? "Any" : priceFormat(value)}
+                  </option>
+                );
+              }
+            )}
           </select>
         </label>
       </StyledFilterBox>
